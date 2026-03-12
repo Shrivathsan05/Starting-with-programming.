3bf352dd3e905ca41fea6d833fa7c6f317f9f806
Shrivathsan05 <guroo1210@gmail.com>
2026-03-12 17:12:30 -0500

    Add files via upload

--- Act.py
+++ Act.py
@@ -1,2 +1,15 @@
-print("Hello, world!")
-print("My name is Shrivathsan.")
+#Print a sentance
+print("Welcome to the world of programming!")
+
+#Print a number
+print(4)
+
+#Next line after print
+print("Hello Python  /n")
+
+#Pass multiple arguments for printing
+print("Hello",5)
+
+#End argument of print statement
+print("Welcome to ", end= "*")
+
