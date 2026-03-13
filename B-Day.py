8138ff469dff4d648a1fd8356b4e6b25e96d2727
Shrivathsan05 <guroo1210@gmail.com>
2026-03-12 19:34:26 -0500

    Add files via upload

---
+++ B-Day.py
@@ -0,0 +1,11 @@
+Raj  = "Raj's Birthday is 1/5/12"
+Guru = "Guru's Birthday is 3/2/17"
+Meena = "Meena's Birthday is 4/3/15"
+Sai = "Sai's Birthday is 2/4/16"
+Jai = "Jai's Birthday is 5/1/09"
+
+print(Raj)
+print(Guru)
+print(Meena)
+print(Sai)
+print(Jai)
