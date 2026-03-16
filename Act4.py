a061e5f63f06e4a48bc1e2100310a1db41cd9143
Shrivathsan05 <guroo1210@gmail.com>
2026-03-16 17:42:42 -0500

    Add files via upload

---
+++ Act4.py
@@ -0,0 +1,19 @@
+#Write a program to change the datatype of given variables?
+# Assigning different variables
+Name = "Penguin"
+Age = 15
+Is_Student = True
+Weight = 38.5
+
+#Printing different variables and their data type
+print("Name :", Name)
+print("Data type of Name is", type(Name))
+
+print("Age :", Age)
+print("Data type of Age is", type(Age))
+
+print("Is_Student :", Is_Student)
+print("Data type of Is_Student is", type(Is_Student))
+
+print("Weight :", Weight)
+print("Data type of Weight is", type(Weight))
