b0c3566b952c155188c71edef7c0a28fcd4476df
Shrivathsan05 <guroo1210@gmail.com>
2026-03-19 19:00:32 -0500

    Add files via upload

---
+++ Act6.py
@@ -0,0 +1,14 @@
+#There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches and writes the height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+Tree1 = 98
+Tree2 = 94
+Tree3 = 42
+Tree4 = 95
+Tree5 = 11
+
+#Finding the total of the trees
+Sum = Tree1+Tree2+Tree3+Tree4+Tree5
+print ("The sum of all the trees are: ", Sum)
+
+#Finding the average of the trees
+Average = Sum/5
+print ("The average of all the trees are: ", Average)
