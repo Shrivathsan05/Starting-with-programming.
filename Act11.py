e220be2fb22ef4e4a0a0540865882d122457529a
Shrivathsan05 <guroo1210@gmail.com>
2026-03-23 17:47:55 -0500

    Add files via upload

---
+++ Act11.py
@@ -0,0 +1,10 @@
+#Write a program to check whether the given number is greater than 15 or smaller than 15.
+I = int(input("Enter a number: "))
+if (I < 15):
+    print ("I is smaller than 15!") 
+    print("I'm in if block!")
+else:
+    print ("I is greater than 15!") 
+    print("I'm in else block!")
+print("I'm not in if & not in else block!")
+
