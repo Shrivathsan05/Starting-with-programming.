4a1821011bf6cbc21ca48900cf57732c043ff19a
Shrivathsan05 <guroo1210@gmail.com>
2026-03-23 17:29:10 -0500

    Add files via upload

---
+++ Act9.py
@@ -0,0 +1,9 @@
+#Write a program to check whether the given number is positive?
+Num = 3
+if Num > 0:
+    print(Num, " is a positive number!")
+
+
+Num = -1
+if Num < 0:
+    print(Num, " is a negative number!")
