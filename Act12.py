b38afc46f4b40ad5b7f2fa5d060e19d25b3f7353
Shrivathsan05 <guroo1210@gmail.com>
2026-03-23 17:53:24 -0500

    Add files via upload

---
+++ Act12.py
@@ -0,0 +1,10 @@
+#Write a program to check whether the given number is even or odd?
+Number = int(input("Enter number to check!"))
+print("Number to be checked: ", Number)
+
+if Number%2==0:
+    print("This is an even number!")
+
+else:
+    print("This is an odd number!")
+
