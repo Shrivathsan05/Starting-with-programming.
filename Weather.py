77169bf08ccdd1b85c394d8450ab4a6a0cced03e
Shrivathsan05 <guroo1210@gmail.com>
2026-03-24 18:09:18 -0500

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,10 @@
+Weather = int(input("What is the weather today? (In F)"))
+if Weather >= 60 :
+    print ("You may wear light clothing if you want to!")
+if 50 <= Weather <= 59 :
+    print ("You must wear a jacket/sweatshirt!")
+if 5 <= Weather <= 49 :
+    print ("You must wear a coat!")
+if Weather <= 4 :
+    print ("DO NOT GO OUT!")
+
