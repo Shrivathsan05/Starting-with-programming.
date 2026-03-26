459ebfbb672cbdd94c0fbdd8b8618e120e42d190
Shrivathsan05 <guroo1210@gmail.com>
2026-03-26 17:01:38 -0500

    Add files via upload

---
+++ Act14.py
@@ -0,0 +1,28 @@
+#Write a program to check the application of not equal operator!
+A = 10
+B = 12
+C = 12
+
+print (A != B)
+print (B != C)
+
+
+A = "Python"
+B = "Coding"
+
+if A != B:
+    print (A, 'And', B, 'are different!')
+
+A = 4
+B = 5
+
+if (A == 1) != (B == 5):
+    print('Hello!')
+
+
+
+
+A = int(input("Enter a number."))
+
+if A%2 != 0:
+    print(A, "is not a even number!")
