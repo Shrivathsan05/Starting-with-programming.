ace0233e1a439383c8e040a38065baf1c271951b
Shrivathsan05 <guroo1210@gmail.com>
2026-03-26 16:45:46 -0500

    Add files via upload

---
+++ Act13.py
@@ -0,0 +1,26 @@
+#Write a program to check whether the given values have boolean values or not.
+A = 10
+B = 12
+C = 0
+
+if A and B and C:
+    print ("All the numbers have Boolean values as true!")
+else:
+    print ("All the numbers have Boolean values as false!")
+
+
+
+A = 10
+B = 10
+C = 0
+
+if A > 0 or B > 0:
+    print ("Either 1 of the numbers is greater than 0!")
+else:
+    print ("Neither of the numbers is greater than 0!")
+
+if B > 0 or C > 0:
+    print ("Either 1 of the numbers is greater than 0!")
+else:
+    print ("Neither of the numbers is greater than 0!")
+
