258d16cdcbf5b787a56cd9d5829ac0883e76a19d
Shrivathsan05 <guroo1210@gmail.com>
2026-03-30 17:41:47 -0500

    Add files via upload

---
+++ Act17.py
@@ -0,0 +1,12 @@
+#Write a program to apply the right shift and left shift bitwise operator.
+A = 10
+B = -10
+#Print bitwise right shift operator
+print ("A >> 1 =", A >> 1)
+print ("B >> 1 =", B >> 1)
+A = 5
+B = -10
+#Print bitwise right shift operator
+print ("A << 1 =", A << 1)
+print ("B << 1 =", B << 1)
+
