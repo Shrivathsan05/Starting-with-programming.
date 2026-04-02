e12745cabe1929d752b149addf3dd7f385ade98d
Shrivathsan05 <guroo1210@gmail.com>
2026-04-02 16:47:34 -0500

    Add files via upload

---
+++ Act19.py
@@ -0,0 +1,15 @@
+#Write a program to understand how the operator precedence works
+V = 4
+W = 5
+X = 8
+Y = 2
+Z = 0
+Z = (V + W) * (X / Y);
+print ("(V + W) * (X / Y) =", Z)
+
+Name = "Arjun"
+Age = 0
+if Name == "Arjun" or "Shri" and Age >= 2 :
+    print("Hello! Welcome!")
+else:
+    print("Good bye!")
