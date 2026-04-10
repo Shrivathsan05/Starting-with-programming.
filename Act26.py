8bed317d18d7bacf8cfdf7eb799728fbfb6ae638
Shrivathsan05 <guroo1210@gmail.com>
2026-04-10 17:49:32 -0500

    Add files via upload

---
+++ Act26.py
@@ -0,0 +1,8 @@
+#Write a program to reverse the string entered by the user.
+String =  input("Please enter your own string : ")
+GnirtsAKAReverseString = ("")
+for Str in String :
+    GnirtsAKAReverseString = Str + GnirtsAKAReverseString
+print("\nThe Original String = ", String)
+print("The Reversed String = ", GnirtsAKAReverseString)
+
