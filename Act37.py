1ee2c9d20c2c8b3e33a8338c0a89f98db751eccc
Shrivathsan05 <guroo1210@gmail.com>
2026-04-23 17:02:11 -0500

    Add files via upload

---
+++ Act37.py
@@ -0,0 +1,20 @@
+#Write a program to draw a star using a turtle?
+import turtle
+turtle.Screen().bgcolor("Red")
+Board = turtle.Turtle()
+Board.forward(100)
+Board.left(120)
+Board.forward(100)
+Board.left(120)
+Board.forward(100)
+Board.penup()
+Board.right(150)
+Board.forward(50)
+Board.pendown()
+Board.right(90)
+Board.forward(100)
+Board.right(120)
+Board.forward(100)
+Board.right(120)
+Board.forward(100)
+turtle.done
