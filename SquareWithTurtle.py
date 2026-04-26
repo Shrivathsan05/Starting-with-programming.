f4bc9d1dabbf2172094a796123a065153dd244ab
Shrivathsan05 <guroo1210@gmail.com>
2026-04-26 18:38:21 -0500

    Add files via upload

---
+++ SquareWithTurtle.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("Red")
+Board = turtle.Turtle()
+Board.forward(100)
+Board.left(90)
+Board.forward(100)
+Board.left(90)
+Board.forward(100)
+Board.left(90)
+Board.forward(100)
+turtle.done
