7264711fd66607d7a37f30e7caeaf9330243005b
Shrivathsan05 <guroo1210@gmail.com>
2026-04-27 18:17:00 -0500

    Add files via upload

---
+++ CircumferenceOfACircle.py
@@ -0,0 +1,4 @@
+import math
+Radius = int ( input (" Enter the radius of the circle you want to find the circumference of, in inches : ") )
+Circumference = math.pi * Radius * 2
+print (" This is the circumference of your circle, in inches :", f"{Circumference:.2f}")
