c4ffe3b3313c5f702007e6028508630189436445
Shrivathsan05 <guroo1210@gmail.com>
2026-04-27 17:31:54 -0500

    Add files via upload

---
+++ Act40.py
@@ -0,0 +1,7 @@
+def WeatherCondition() :
+    print ("The weather is pleasant in : ", Spring)
+    print ("The weather is the same in : ", Autumn)
+Spring = "Autumn"
+Autumn = Spring
+WeatherCondition()
+
