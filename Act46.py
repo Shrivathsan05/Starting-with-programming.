09ccd9a5c3b05a6f79f515bbb2739134f4876246
Shrivathsan05 <guroo1210@gmail.com>
2026-05-07 17:02:45 -0500

    Add files via upload

---
+++ Act46.py
@@ -0,0 +1,11 @@
+for s in range ( 10 ) : 
+    if s % 20 == 0 : 
+        print ( " Twist! " ) 
+    elif s % 15 == 0 : 
+        pass 
+    elif s % 5 == 0 : 
+        print ( " Fizz! " )
+    elif s % 3 == 0 : 
+        print ( " Buzz! " )
+    else : 
+        print ( s )
---
+++ Act47.py
@@ -0,0 +1,7 @@
+Variable = 10
+while Variable > 0 : 
+    Variable = Variable - 1
+    if Variable == 5 : 
+        continue
+    print ( " \nCurrent Variable Value : ", Variable )
+print ( " \nGood Bye! " )
