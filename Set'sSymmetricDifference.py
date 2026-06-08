de3d27ca4f0f9d39ef32b58602acd6a5de96aeab
Shrivathsan05 <guroo1210@gmail.com>
2026-06-08 16:07:08 -0500

    Add files via upload

---
+++ Set'sSymmetricDifference.py
@@ -0,0 +1,6 @@
+Set1 = {" Hello! ", 67.76, 41, True, 21, False, 38, 76.67, " Bye! "}
+Set2 = {" Hello. ", 6.7, 7.6, 41, True, 2.1, False, 38, 76.67, " Bye bye! "}
+print(" Set1 =", Set1, "" )
+print(" Set2 =", Set2, "" )
+SetsSymmetricDifference = Set1.symmetric_difference(Set2)
+print(" The set's symmetric difference is =", SetsSymmetricDifference, "" )
