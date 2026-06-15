6428b1d7bf8c80b827836e053bc4a747a4c90640
Shrivathsan05 <guroo1210@gmail.com>
2026-06-15 17:35:44 -0500

    Add files via upload

---
+++ Act65JKAct56.py
@@ -0,0 +1,5 @@
+#Write a program to create a class with the name Student and perform the following tasks - 1. Declare a variable grade 2. Print a sentence inside the class 3. Create an object of class student and see the output
+class Student :
+    Grade = 10
+    print(" Hi I am a student of grade", Grade, "! ")
+Object = Student()
