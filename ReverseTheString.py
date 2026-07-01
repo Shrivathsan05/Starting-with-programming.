4abe54a75b987d5ed2ef1a480c76894bfed89e08
Shrivathsan05 <guroo1210@gmail.com>
2026-07-01 18:34:55 -0500

    Add files via upload

---
+++ ReverseTheString.py
@@ -0,0 +1,12 @@
+NormalString = str(input(" Please enter a, 7 OR LESS! DIGIT! WORD! "))
+while len(NormalString) > 7 :
+    NormalString = str(input(" Please enter a, 7 OR LESS! DIGIT! WORD! "))
+print(" Thank you for entering a, WORD! That is only, 7 OR LESS DIGITS! ")
+class Reverse : 
+    def __init__(Self) :
+        Self.String = "S"
+    def ReverseTheString(Self, S) :
+        return S[::-1]
+ReversedString = Reverse()
+ReversedString = ReversedString.ReverseTheString(NormalString)
+print(ReversedString)
